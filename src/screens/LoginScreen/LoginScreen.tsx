4295c32d89451e144e29e4d6ed953b547abbd912
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-07-17 14:27:13 +0700

    Login and Register screen UI

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,11 +1,111 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackNavigationProp } from "../../navigation/types";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation<RootStackNavigationProp>();
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
+      <KeyboardAvoidingView>
+        <View
+          style={{
+            marginTop: 40,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}>
+            Sign In
+          </Text>
+          <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+            Sign In to Your Account
+          </Text>
+        </View>
+        <View style={{ marginTop: 50 }}>
+          <View>
+            <Text>Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your email"
+            />
+          </View>
+          <View>
+            <Text>Password</Text>
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your password"
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 200,
+            backgroundColor: "#4A55A2",
+            padding: 15,
+            marginTop: 50,
+            marginLeft: "auto",
+            marginRight: "auto",
+            borderRadius: 6,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("RegisterScreen")}
+          style={{ marginTop: 15 }}
+        >
+          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+            Don't have an account? Sign Up
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
--- src/screens/RegisterScreen/RegisterScreen.tsx
+++ src/screens/RegisterScreen/RegisterScreen.tsx
@@ -1,11 +1,144 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackNavigationProp } from "../../navigation/types";
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState("");
+  const navigation = useNavigation<RootStackNavigationProp>();
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        padding: 10,
+        alignItems: "center",
+      }}
+    >
+      <KeyboardAvoidingView>
+        <View
+          style={{
+            marginTop: 40,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}>
+            Register
+          </Text>
+          <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+            Register to Your Account
+          </Text>
+        </View>
+        <View style={{ marginTop: 50 }}>
+          <View>
+            <Text>Name</Text>
+            <TextInput
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your name"
+            />
+          </View>
+          <View>
+            <Text>Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your email"
+            />
+          </View>
+          <View>
+            <Text>Password</Text>
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your password"
+            />
+          </View>
+          <View>
+            <Text>Image</Text>
+            <TextInput
+              value={image}
+              onChangeText={(text) => setImage(text)}
+              style={{
+                fontSize: 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300,
+              }}
+              // placeholderTextColor={"black"}
+              placeholder="Enter your Image"
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 200,
+            backgroundColor: "#4A55A2",
+            padding: 15,
+            marginTop: 50,
+            marginLeft: "auto",
+            marginRight: "auto",
+            borderRadius: 6,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            Register
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{ marginTop: 15 }}
+        >
+          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+            Already Have an account? Sign in
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
